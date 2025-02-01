d7985fd55506cc1cce7f77b760f6699c9bf00c26
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2025-02-01 23:43:57 +0545

    implemented user specific crud  operations

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,15 +4,15 @@ const { Connection } = require("mongoose");
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find();
+    const contacts = await Contact.find({user_id: req.user.id});
     res.status(200).json(contacts);
 });
 
 //@desc Create new contact
 //@route POST api/contacts
-//@access public
+//@access private
 const createContact = asyncHandler(async (req, res) => {
     const {name, email, phone} = req.body;
     if (!name || !email || !phone) {
@@ -22,14 +22,15 @@ const createContact = asyncHandler(async (req, res) => {
     const contact = await Contact.create({
         name,
         email,
-        phone
+        phone,
+        user_id: req.user.id
     });
     res.status(201).json(contact);
 });
 
 //@desc Get contact
 //@route GET api/contacts/:id
-//@access public
+//@access private
 const getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id)
     if(!contact){
@@ -41,7 +42,7 @@ const getContact = asyncHandler(async (req, res) => {
 
 //@desc Update contact
 //@route PUT api/contacts/:id
-//@access public
+//@access private
 const updateContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact){
