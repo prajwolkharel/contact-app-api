a5000de2a7476708e1f37f74d4ecf1faeb24dd1b
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2025-02-01 23:03:33 +0545

    implemented user login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //@desc Register a user
 //@route POST /api/users/register
@@ -32,7 +33,31 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message:"Login user"});
+    const {email, password} = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!")
+    }
+
+    const user = await User.findOne({email});
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+        {
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: "1m"}
+        );
+        res.status(200).json({accessToken});
+    }
+    else {
+        res.status(401);
+        throw new Error("Email or password is not valid");
+    }
 });
 
 //@desc Current user info
