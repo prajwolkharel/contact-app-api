460d39cc97f28da2035672cceafc76d67e880f51
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2025-02-01 22:37:18 +0545

    implemented register user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,31 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
+    const {username, email, password} = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory")
+    }
 
-    res.status(201).json({message:"Register user"});
+    const userAvailable = await User.findOne({email});
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered");
+    };
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    res.status(200).json({_id: user.id, email: user.email});
 });
 
 //@desc Login a user
